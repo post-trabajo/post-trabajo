4741bc77cf48c712981b07da472f6e33cd5f23dd
jose <jose12_bernal@hotmail.com>
2024-02-20 13:03:28 -0700

    route added in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express'
+import UserRouter from './routes/routes'
 
 const app = express()
-
+app.use('/api/user',UserRouter )
 
 app.listen(8080, () => {
   console.log('server listening to port 8080')
